d480a6f5e8cc8b8135021a4c2ef6b5e8e6af3c19
notGonim <mhmodgonim96@gmail.com>
2021-01-06 13:53:17 +0200

    Adding : bottom sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,7 +4,16 @@ import Avatar from '@material-ui/core/Avatar';
 
 
 export const Sidebar = () => {
+
+    const recentItem = topic => (
+        <div className="sidebar_recentItem">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
     return (
+
         <div className="sidebar">
             <div className="sidebar_top">
                 <img src="https://media-exp1.licdn.com/dms/image/C4D16AQHWPd86umKAew/profile-displaybackgroundimage-shrink_200_800/0/1604838172903?e=1615420800&v=beta&t=-72yuvdX3Wm3JZgSqE9ilJ8mMPeoqkAD34QX3J1RnDc" alt="bg-img" />
@@ -18,18 +27,29 @@ export const Sidebar = () => {
                     <p>Who Viewed You</p>
                     <p className="sidebar_startNumber">
                         2.568
-                    </p>
+            </p>
                 </div>
                 <div className="sidebar_start">
                     <p>Views on posts</p>
                     <p className="sidebar_startNumber">
                         7.895
-                    </p>
+            </p>
                 </div>
             </div>
-            <div className="sidebar_buttons">
+            <div className="sidebar_bottom">
                 <p>Recent</p>
+                {recentItem('Programming')}
+                {recentItem("reactJS")}
+                {recentItem("SoftwareEngineering")}
+                {recentItem("design")}
+                {recentItem("developer")}
             </div>
         </div>
-    )
+
+
+    );
+
+
+
 }
+
