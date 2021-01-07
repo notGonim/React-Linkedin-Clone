dd3dc3f98535785301ee4f6e274a0f10c8ff7b1b
notGonim <mhmodgonim96@gmail.com>
2021-01-07 10:54:51 +0200

    Done : with form feed at home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/header/Header';
 import { Sidebar } from './components/sidebar/Sidebar';
+import { Feed } from './components/feed/Feed';
 
 function App() {
   return (
     <div className="App">
       <Header />
-    <div className="app_body">
-      <Sidebar />
-    </div>
+      <div className="app_body">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
---
+++ src/components/feed/Feed.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import CreateIcon from '@material-ui/icons/Create';
+import './feed.css'
+import { InputOption } from './InputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+
+export const Feed = () => {
+    return (
+        <div className="feed">
+            <div className="feed_inputContainer">
+                <div className="feed_input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" name="" id="" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed_inputOptions">
+                    <InputOption title="Photo" Icon={ImageIcon} color="#70B5f9" />
+                    <InputOption title="Video" Icon={SubscriptionsIcon} color="#E7A33E" />
+                    <InputOption title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
+                    <InputOption title="Write article" Icon={CalendarViewDayIcon} color="#7FC15E" />
+                </div>
+            </div>
+
+        </div>
+    )
+}
